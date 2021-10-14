3074649399d25cd174b4f8be035820338a1262b8
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-14 00:59:47 -0400

    working on the react-groceries

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import groceries from './groceries-data';
+
 
 function App() {
+
+  const [bought,setBought]=useState([])
+  //  console.log(setBought);
+  const delItem=(e)=>{
+     console.log(e.value);
+  }
+
+
+  console.log(groceries);
+  let prices=0;
+  groceries.map((hh)=>prices+=Number(hh.price))
+  console.log(prices);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grocery-app">
+      
+      <h1>My Groceries</h1>
+      {groceries.map((hh,value)=>(
+        <div className='grocery-list' key={value} >
+         <h4>{hh.item} - {hh.brand}</h4>
+         <h4>{hh.quantity}</h4>
+         <h4>{hh.price}$</h4>
+         <button onClick={(e)=>delItem(e)} >click me</button>
+         </div>
+      ))}
+      
+        <p> Total : {prices}</p>
+      
     </div>
   );
 }
---
+++ my-blog/src/groceries-data.js
@@ -0,0 +1,41 @@
+const groceries = [
+  {
+    item: 'Flour',
+    brand: 'Golden Start',
+    price: '12',
+    quantity: '1lb',
+    isPurchased: false
+  },
+  {
+    item: 'Gold',
+    brand: 'My Start',
+    price: '24',
+    quantity: '1lb',
+    isPurchased: false
+  },
+  {
+    item: 'Fish',
+    brand: 'Fish Market',
+    price: '112',
+    quantity: '11lb',
+    isPurchased: false
+  },
+  {
+    item: 'Wheat',
+    brand: 'Pak Store',
+    price: '22',
+    quantity: '2lb',
+    isPurchased: false
+  },
+  {
+    item: 'feather',
+    brand: 'myfeath3er',
+    price: '122',
+    quantity: '2lb',
+    isPurchased: false
+  }
+
+ 
+]
+
+export default groceries;
